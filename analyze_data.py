9362bdb5838126d31c664f02aca351f9ea47efff
Eric Hwang <eric_hwang@college.harvard.edu>
2025-04-18 00:48:37 -0400

    Analyzed each era of kickoffs

--- analyze_data.py
+++ analyze_data.py
@@ -1,14 +1,21 @@
 import pandas as pd 
+import os
 
 INPUT_FILE = './modified_data/kickoff_plays.csv'
 OUTPUT_FILE = './results/kickoff_returners_1999.csv'
-GROUPED_OUTPUT_FILE = './results/grouped_kickoff_returners_1999.csv'
+GROUPED_OUTPUT_FILE = './results/grouped_kickoff_returners.csv'
 
-def calculate_avg_epa():
-    data = pd.read_csv(INPUT_FILE, dtype={"lateral_kickoff_returner_player_id": str, "lateral_kickoff_returner_player_name": str, "own_kickoff_recovery_player_id": str, "own_kickoff_recovery_player_name": str})
+def calculate_avg_epa(input_files):
+
+    data_frames = []
+    for input_file in input_files:
+        df = pd.read_csv(input_file, dtype={"lateral_kickoff_returner_player_id": str, "lateral_kickoff_returner_player_name": str, "own_kickoff_recovery_player_id": str, "own_kickoff_recovery_player_name": str})
+        data_frames.append(df)
+    
+    data = pd.concat(data_frames, ignore_index=True)
     data = data.sort_values(by="kickoff_returner_player_id")
     data.to_csv(GROUPED_OUTPUT_FILE, index=False)
-    
+        
     result = data.groupby("kickoff_returner_player_id").agg(
         avg_ep=("ep", "mean"),
         avg_epa=("epa", "mean"),
@@ -22,8 +29,34 @@ def calculate_avg_epa():
     )
     filtered_result = result[result["return_count"] >= 20]
     print(filtered_result.nlargest(5, "avg_epa"))
-    result.to_csv(OUTPUT_FILE, index=False)
+    return result
+
+
 
 if __name__ == "__main__":
-    calculate_avg_epa()
-    print(f"Average EPA and EP for each kickoff returner saved to {OUTPUT_FILE}")
+    cleaned_data_dir = './modified_data'
+    eras = {"1999_to_2010": [], "2011_to_2015": [], "2016_to_2023": [], "2024": []}
+    
+    for file_name in os.listdir(cleaned_data_dir):
+        if file_name.endswith('.csv'):
+            input_file = os.path.join(cleaned_data_dir, file_name)
+
+            print(f"Processing file: {input_file}")
+
+            year = int(file_name.split('_')[-1].split('.')[0])
+            if year <= 2010:
+                eras["1999_to_2010"].append(input_file)
+            elif year <= 2015:
+                eras["2011_to_2015"].append(input_file)
+            elif year <= 2023:
+                eras["2016_to_2023"].append(input_file)
+            else:
+                eras["2024"].append(input_file)
+
+    for era, input_files in eras.items():
+        print(f"Processing era: {era}")
+        result = calculate_avg_epa(input_files)
+        output_file = os.path.join('./results', f'kickoff_returners_{era}.csv')
+        result.to_csv(output_file, index=False)
+        print(f"Average EPA and EP for each kickoff returner saved to {output_file}")
+    
--- clean_data.py
+++ clean_data.py
@@ -1,9 +1,10 @@
 import pandas as pd
+import os
 
 INPUT_FILE = './raw_data/play_by_play_1999.csv'
-OUTPUT_FILE = './modified_data/kickoff_plays.csv'
+OUTPUT_FILE = './modified_data/kickoff_plays_1999.csv'
 
-def remove_unecessary_data():
+def remove_unecessary_data(input_file):
     # Load the data
     columns_to_read = [
         'play_id', 'game_id', 'old_game_id', 'home_team', 'away_team', 'season_type', 'week', 
@@ -18,7 +19,7 @@ def remove_unecessary_data():
         'own_kickoff_recovery_player_name', 'return_team', 'return_yards', 'penalty_team', 
         'penalty_player_id', 'penalty_player_name', 'penalty_yards', 'penalty_type', 'location'
     ]
-    data = pd.read_csv(INPUT_FILE, usecols=columns_to_read, dtype={"lateral_kickoff_returner_player_id": str, "lateral_kickoff_returner_player_name": str, "own_kickoff_recovery_player_id": str, "own_kickoff_recovery_player_name": str})
+    data = pd.read_csv(input_file, usecols=columns_to_read, dtype={"lateral_kickoff_returner_player_id": str, "lateral_kickoff_returner_player_name": str, "own_kickoff_recovery_player_id": str, "own_kickoff_recovery_player_name": str})
 
     # Filter rows where play_type is "kickoff"
     kickoff_data = data[data['play_type'] == 'kickoff']
@@ -26,9 +27,27 @@ def remove_unecessary_data():
    
 
 if __name__ == "__main__":
-    kickoff_data = remove_unecessary_data()
-    print(kickoff_data.head())
-    # Save the filtered data to 
-    # a new CSV
-    kickoff_data.to_csv(OUTPUT_FILE, index=False)
-    print(f"Kickoff data saved to {OUTPUT_FILE}")
+    raw_data_dir = './raw_data'
+
+    for file_name in os.listdir(raw_data_dir):
+        if file_name.endswith('.csv'):
+            input_file = os.path.join(raw_data_dir, file_name)
+            year = file_name.split('_')[-1].split('.')[0]
+
+            print(f"Processing file: {input_file}")
+            data = remove_unecessary_data(input_file)
+
+            output_file = os.path.join('./modified_data', f'kickoff_plays_{year}.csv')
+            data.to_csv(output_file, index=False)
+            print(f"Filtered data saved to {output_file}")
+    
+    
+    
+
+
+    # kickoff_data = remove_unecessary_data()
+    # print(kickoff_data.head())
+    # # Save the filtered data to 
+    # # a new CSV
+    # kickoff_data.to_csv(OUTPUT_FILE, index=False)
+    # print(f"Kickoff data saved to {OUTPUT_FILE}")
